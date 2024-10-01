05b2079cbd213b507bda3071d2364323ff1b1522
tomyhas59 <yh9035926@naver.com>
2024-10-01 09:59:32 +0900

    home

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import Main from "./pages/Main";
 import Annual from "./pages/Annual";
 import { RecoilRoot } from "recoil";
 import Sign from "./pages/Sign";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <RecoilRoot>
       <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/main" element={<Main />} />
           <Route path="/annual" element={<Annual />} />
-          <Route path="/" element={<Sign />} />
+          <Route path="/login" element={<Sign />} />
         </Routes>
       </BrowserRouter>
     </RecoilRoot>
---
+++ src/pages/Home.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: #f0f8ff;
+`;
+
+const Title = styled.h1`
+  font-size: 3rem;
+  color: #2c3e50;
+  margin-bottom: 20px;
+`;
+
+const StyledLink = styled(Link)`
+  font-size: 1.5rem;
+  color: white;
+  background-color: #3498db;
+  padding: 10px 20px;
+  border-radius: 5px;
+  text-decoration: none;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #2980b9; /* 마우스 올릴 때 색상 변화 */
+  }
+`;
+
+const Home = () => {
+  return (
+    <Container>
+      <Title>가계부</Title>
+      <StyledLink to="/login">로그인하기</StyledLink>
+    </Container>
+  );
+};
+
+export default Home;
