f576c242897981f616b3306600a7376f18570fe9
tomyhas59 <yh9035926@naver.com>
2024-10-13 07:45:46 +0900

    BASE_URL cloud

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
 export const BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "https://your-production-url.com"
+    ? "https://port-0-household-book-server-rccln2llvsdixmg.sel5.cloudtype.app"
     : "http://localhost:8090";
