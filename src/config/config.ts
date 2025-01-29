3c22975d6b82befe59b5fdf1e47047ada9cb6a15
이용현 <yh9035926@naver.com>
2025-01-30 08:00:29 +0900

    BASE_URL update

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,4 @@
 export const BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "https://port-0-household-book-server-rccln2llvsdixmg.sel5.cloudtype.app"
+    ? "https://port-0-household-book-back-rccln2llvsdixmg.sel5.cloudtype.app"
     : "http://localhost:8090";
